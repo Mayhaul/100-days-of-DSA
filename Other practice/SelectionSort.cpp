40ce6d5ac4d0b9c1a4e1dd71e1a9606571d94473
Mayhaul <mehulv2k6@gmail.com>
2025-07-15 15:42:06 +0530

    SelectionSort

---
+++ Other practice/SelectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+vector<int> nums = {7, 3,2,5};
+int n = nums.size();
+int j = 0;
+
+
+for(int i = 0; i<n-1; i++){
+int Min = nums[i];
+int minIndex = i;
+
+    for(int j = i+1;j<n;j++){
+    Min = min(Min,nums[j]);
+    
+    if(Min == nums[j]){
+        minIndex = j;
+    }
+    
+}
+        int temp = nums[i];
+        nums[i] = nums[minIndex];
+        nums[minIndex] = temp;
+    
+}
+for(int num : nums){
+    cout<<num;
+}
+    return 0;
+}
